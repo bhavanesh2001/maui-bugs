b9becee78d9762f44fd37d7e7bd095a433c28802
Bhavanesh N <bhavanesh001@gmail.com>
2025-06-11 19:48:15 +0530

    29937 repro

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,24 +1,40 @@
-﻿namespace maui_bugs
+﻿using System.Collections.ObjectModel;
+
+namespace maui_bugs
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
+        private ObservableCollection<string>? _items;
+
+        public ObservableCollection<string>? Items
+        {
+            get => _items;
+            set
+            {
+                _items = value;
+                OnPropertyChanged(nameof(Items));
+            }
+        }
 
         public MainPage()
         {
             InitializeComponent();
+            Items = new ObservableCollection<string>
+            {
+                "Item 1",
+                "Item 2",
+                "Item 3",
+                "Item 4",
+                "Item 5"
+            };
+            BindingContext = this;
         }
 
-        private void OnCounterClicked(object? sender, EventArgs e)
+        private void Button_Clicked(object sender, EventArgs e)
         {
-            count++;
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
+            cView.SelectedItem = "Item 1";
+            cView.ItemsSource = null;
         }
+       
     }
 }
