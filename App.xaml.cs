0815bae50331596be10e8d1893e67fb2ef6c9410
bhavanesh001 <bhavanesh001@gmail.com>
2025-06-20 01:12:02 +0530

    stuff

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,7 +1,12 @@
-﻿namespace maui_bugs
+﻿using System.Diagnostics;
+using Microsoft.Maui.Controls;
+
+namespace maui_bugs
 {
     public partial class App : Application
     {
+        private WeakReference? _leakyPageReference;
+
         public App()
         {
             InitializeComponent();
@@ -9,7 +14,66 @@
 
         protected override Window CreateWindow(IActivationState? activationState)
         {
-            return new Window(new AppShell());
+            return new Window(GetNavigationPage());
+        }
+
+        private static ContentPage CreateRootPage()
+        {
+            return new ContentPage
+            {
+                Title = "Root",
+                Content = new VerticalStackLayout
+                {
+                    Children =
+                    {
+                        new Label { Text = "Root Page" },
+                        new Button
+                        {
+                            Text = "Push Leaky Page",
+                            Command = new Command(async () =>
+                            {
+                                if (Application.Current?.Windows[0].Page is NavigationPage nav)
+                                {
+                                    var page = new LeakyPage();
+                                    await nav.PushAsync(page);
+                                }
+                            })
+                        }
+                    }
+                }
+            };
+        }
+
+        public static  NavigationPage GetNavigationPage()
+        {
+          return new NavigationPage(CreateRootPage());
+
+        }
+
+        public class LeakyPage : ContentPage
+        {
+            public LeakyPage()
+            {
+                Title = "LeakyPage";
+                Content = new VerticalStackLayout
+                {
+                    Children =
+                    {
+                        new Label { Text = "I'm supposed to be gone!" },
+                        new Button
+                        {
+                            Text = "Replace Window.Page",
+                            Command = new Command(() =>
+                            {
+                                if(App.Current?.Windows[0] is Window currentWindow)
+                                {
+                                    currentWindow.Page = App.GetNavigationPage();
+                                }
+                            })
+                        }
+                    }
+                };
+            }
         }
     }
 }
