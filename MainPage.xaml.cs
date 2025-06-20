acb9fc567bab1492ebbc5c59a787c64f296676b1
bhavanesh001 <bhavanesh001@gmail.com>
2025-06-20 14:33:32 +0530

    repro

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,10 +1,27 @@
-﻿namespace maui_bugs
+﻿using System.Diagnostics;
+
+namespace maui_bugs
 {
     public partial class MainPage : ContentPage
     {
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        private void Entry_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            Debug.WriteLine($"Old Value: {e.OldTextValue}, New Value: {e.NewTextValue}");
+        }
+
+        private void Editor_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            Debug.WriteLine($"Old Value: {e.OldTextValue}, New Value: {e.NewTextValue}");
+        }
+
+        private void SearchBar_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            Debug.WriteLine($"Old Vlaue: {e.OldTextValue}, New Value: {e.NewTextValue}");
         }
     }
 }
