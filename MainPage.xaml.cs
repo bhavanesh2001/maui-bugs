83209960e3b4ec973e467de339da4ea65152d1ea
bhavanesh001 <bhavanesh001@gmail.com>
2025-07-25 14:32:13 +0530

    bug

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,10 +1,22 @@
-﻿namespace maui_bugs
+﻿using System.Threading.Tasks;
+
+namespace maui_bugs
 {
     public partial class MainPage : ContentPage
     {
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new NewPage());
         }
     }
 }
---
+++ NewPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace maui_bugs;
+
+public partial class NewPage : ContentPage
+{
+	public NewPage()
+	{
+		InitializeComponent();
+	}
+}
